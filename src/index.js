89078095a41c86d4c21900c4357ed6214a6bf1e5
Zihao1319 <zihao1319@gmail.com>
2022-09-19 23:52:11 +0800

    1st commit frontend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Sighting from "./Sighting";
+import SightingPreviewList from "./SightingPreviewList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      {/* Route that provides base app UI */}
+      <Route path="/" element={<App />}>
+        {/* Route that renders all sightings */}
+        <Route index element={<SightingPreviewList />} />
+        {/* Route that renders individual sightings */}
+        <Route path="sightings/:sightingIndex" element={<Sighting />} />
+        {/* Route that matches all other paths */}
+        <Route path="*" element={"Nothing here!"} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
